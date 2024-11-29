76da8eeb3e7b695fa8f3571557a7713e352e495b
VU-TUAN-KIET-BENGO4 <k.vu@bengo4.com>
2024-11-29 16:05:15 +0900

    tmp commit

---
+++ src/components/UserProfile.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="user-profile">
+    <UserAvatar />
+    <h2>相手の名前</h2>
+    <p>役員・法務事務所</p>
+    <p>性別</p>
+    <p>連絡先</p>
+  </div>
+</template>
+
+<script setup lang="ts">
+import UserAvatar from "./UserAvatar.vue";
+</script>
+
+<style lang="css" scoped>
+.user-profile {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+  margin-top: 1rem;
+}
+
+.user-profile h2 {
+  font-size: 32px;
+}
+
+.user-profile p {
+  font-size: 24px;
+}
+</style>
