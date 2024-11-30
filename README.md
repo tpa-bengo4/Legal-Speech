55120b8561b090b53b579f4ae0ef9f3f69af0ec7
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2024-11-30 14:46:29 +0900

    update config router

--- README.md
+++ README.md
@@ -1,8 +1,6 @@
 # Vue 3 + TypeScript + Vite
 
-
 ![DALL·E 2024-11-29 00 30 27 - A simple logo design for a speech-to-text functionality with a legal theme  The logo features a minimalistic microphone icon with a single sound wave  copy 2](https://github.com/user-attachments/assets/04be0d43-32af-40dd-a42e-6fe6849a1cb4)
-
 
 ## 現状の問題点
 
--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,14 @@ import { RouteRecordRaw, createWebHistory, createRouter } from "vue-router";
 import { VIEW_CONFIG } from "@/constants";
 
 const routes: Array<RouteRecordRaw> = [
-    
+  {
+    ...VIEW_CONFIG.CONSULTATION_LIST,
+    component: () => import("@/views/ConsultationListScreen.vue"),
+  },
+  {
+    ...VIEW_CONFIG.CONSULTATION,
+    component: () => import("@/views/ConsultationScreen.vue"),
+  },
 ];
 
 const router = createRouter({
