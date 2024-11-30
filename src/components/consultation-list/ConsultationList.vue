654a7c03bbc18d4ec3719afeac6a60888772c124
Bui_Danh_Tung <bui.danh.tung@sun-asterisk.com>
2024-12-01 02:02:17 +0900

    add component files

--- src/components/consultation-list/ConsultationList.vue
+++ src/components/consultation-list/ConsultationList.vue
@@ -1,3 +1,13 @@
-<script setup lang="ts"></script>
-<template>consultation list</template>
-<style scoped></style>
+<script setup lang="ts">
+import ConsultationListFilter from "@/components/consultation-list/ConsultationListFilter.vue";
+import ConsultationListItem from "@/components/consultation-list/ConsultationListItem.vue";
+</script>
+
+<template>
+<div class="container">
+    <ConsultationListFilter />
+</div>
+</template>
+<style scoped>
+
+</style>
--- src/views/ConsultationListScreen.vue
+++ src/views/ConsultationListScreen.vue
@@ -1,3 +1,17 @@
-<script setup lang="ts"></script>
-<template>consultation list</template>
-<style scoped></style>
+<script setup lang="ts">
+import ConsultationList from "@/components/consultation-list/ConsultationList.vue";
+
+</script>
+<template>
+<div class="container">
+    <ConsultationList />
+    
+</div>
+</template>
+<style scoped>
+.container {
+    width: 80vw;
+    height: 80vh;
+    background-color: #fff;
+}
+</style>
