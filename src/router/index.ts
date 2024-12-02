73342363b1b42e864b57b49136218c8750b5f155
BUI-DANH-TUNG-BENGO4 <134197942+BUI-DANH-TUNG-BENGO4@users.noreply.github.com>
2024-12-02 15:03:18 +0900

    Update index.ts

--- src/router/index.ts
+++ src/router/index.ts
@@ -13,7 +13,7 @@ const routes: Array<RouteRecordRaw> = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHistory("/legal-speech"),
   routes,
 });
 
