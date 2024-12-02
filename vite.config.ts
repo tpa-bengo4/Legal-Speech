88ed901ac45b3c386c61b3fa1c95cc8f9f00ecdc
BUI-DANH-TUNG-BENGO4 <134197942+BUI-DANH-TUNG-BENGO4@users.noreply.github.com>
2024-12-02 15:00:58 +0900

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  baseUrl: "/legal-speech/",
   resolve: {
     extensions: [
       ".mjs",
