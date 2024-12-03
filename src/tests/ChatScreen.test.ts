6db66b69ccb34f2cf03b1e5a13784349a2f79f52
VU-TUAN-KIET-BENGO4 <k.vu@bengo4.com>
2024-12-03 11:32:16 +0900

    tmp

---
+++ src/tests/ChatScreen.test.ts
@@ -0,0 +1,34 @@
+import { beforeEach, describe, expect, it, vi } from "vitest";
+import { mount } from "@vue/test-utils";
+import { useRoute } from "vue-router";
+import ChatFrame from "@/components/chat/ChatFrame.vue";
+
+vi.mock("vue-router");
+
+describe("ChatScreen", () => {
+  let routeMock;
+
+  beforeEach(() => {
+    routeMock = {
+      params: { partnerId: "12" }, // Mock the route parameters
+    };
+
+    useRoute.mockReturnValue(routeMock);
+  });
+
+  it("チャット部分の上に、ユーザー情報があるがある", () => {
+    const wrapper = mount(ChatFrame);
+    expect(wrapper.find("img").attributes("src")).not.toBeNull();
+    expect(wrapper.find(".header h2").text()).toBeTruthy();
+  });
+
+  it("チャット部分に、メッセージがある", () => {
+    const wrapper = mount(ChatFrame);
+    expect(wrapper.find(".msg-page .message").exists()).toBeTruthy();
+
+    const allMessages = wrapper.findAll(".msg-page .message");
+    allMessages.map((message) => {
+      expect(message.text()).toBeTruthy();
+    });
+  });
+});
