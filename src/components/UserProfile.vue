4218e51f2b3047e0a97c730d5dbb0c2af7f799e7
VU-TUAN-KIET-BENGO4 <k.vu@bengo4.com>
2024-12-03 12:15:44 +0900

    done

--- src/components/UserProfile.vue
+++ src/components/UserProfile.vue
@@ -1,37 +1,32 @@
 <template>
   <div class="user-profile">
-    <UserAvatar :source-avatar="user.profilePicture" :status="user.status" :height="'200px'" :width="'200px'"/>
+    <UserAvatar
+      :source-avatar="user.profilePicture"
+      :status="user.status"
+      :height="'200px'"
+      :width="'200px'"
+    />
     <h2>{{ user.name }}</h2>
     <p>役員・法務事務所</p>
     <p>性別</p>
     <p>連絡先</p>
-    <UserProfileDropdown
-      :icon-link="linkIcon"
-      text="リンク"
-    />
-    <UserProfileDropdown
-      :icon-link="fileIcon"
-      text="ファイル"
-    />
-    <UserProfileDropdown
-      :icon-link="mediaIcon"
-      text="画像・ビデオ"
-    />
+    <UserProfileDropdown :icon-link="linkIcon" text="リンク" />
+    <UserProfileDropdown :icon-link="fileIcon" text="ファイル" />
+    <UserProfileDropdown :icon-link="mediaIcon" text="画像・ビデオ" />
   </div>
 </template>
 
 <script setup lang="ts">
 import UserAvatar from "./UserAvatar.vue";
 import UserProfileDropdown from "./UserProfileDropdown.vue";
-import linkIcon from "@/assets/images/link-svgrepo-com.svg"
-import fileIcon from "@/assets/images/file-svgrepo-com.svg"
-import mediaIcon from "@/assets/images/image-svgrepo-com.svg"
+import linkIcon from "@/assets/images/link-svgrepo-com.svg";
+import fileIcon from "@/assets/images/file-svgrepo-com.svg";
+import mediaIcon from "@/assets/images/image-svgrepo-com.svg";
 import { Consultation } from "@/types";
 
-const {user} = defineProps<{
-  user: Consultation
-}>()
-
+const { user } = defineProps<{
+  user: Consultation;
+}>();
 </script>
 
 <style lang="css" scoped>
@@ -41,6 +36,7 @@ const {user} = defineProps<{
   align-items: center;
   gap: 0.5rem;
   margin: 2rem;
+  color: black;
 }
 
 .user-profile h2 {
--- src/tests/ChatScreen.test.ts
+++ src/tests/ChatScreen.test.ts
@@ -2,6 +2,9 @@ import { beforeEach, describe, expect, it, vi } from "vitest";
 import { mount } from "@vue/test-utils";
 import { useRoute } from "vue-router";
 import ChatFrame from "@/components/chat/ChatFrame.vue";
+import consultationData from "@/assets/json/consultations-list.json";
+import messageData from "@/assets/json/consultation-messages.json";
+import UserProfile from "@/components/UserProfile.vue";
 
 vi.mock("vue-router");
 
@@ -17,13 +20,24 @@ describe("ChatScreen", () => {
   });
 
   it("チャット部分の上に、ユーザー情報があるがある", () => {
-    const wrapper = mount(ChatFrame);
+    const wrapper = mount(ChatFrame, {
+      props: {
+        partnerInfo: consultationData.consultations[0],
+        messages: messageData[0].messages,
+      },
+    });
+    expect(wrapper.find("img").exists()).toBeTruthy();
     expect(wrapper.find("img").attributes("src")).not.toBeNull();
     expect(wrapper.find(".header h2").text()).toBeTruthy();
   });
 
   it("チャット部分に、メッセージがある", () => {
-    const wrapper = mount(ChatFrame);
+    const wrapper = mount(ChatFrame, {
+      props: {
+        partnerInfo: consultationData.consultations[0],
+        messages: messageData[0].messages,
+      },
+    });
     expect(wrapper.find(".msg-page .message").exists()).toBeTruthy();
 
     const allMessages = wrapper.findAll(".msg-page .message");
@@ -33,8 +47,29 @@ describe("ChatScreen", () => {
   });
 
   it("チャットの部分に、インプットとリーコードボタンがある", () => {
-    const wrapper = mount(ChatFrame);
+    const wrapper = mount(ChatFrame, {
+      props: {
+        partnerInfo: consultationData.consultations[0],
+        messages: messageData[0].messages,
+      },
+    });
     expect(wrapper.find(".bottom input").exists()).toBeTruthy();
     expect(wrapper.find(".bottom .voice-button").exists).toBeTruthy();
+  });
+
+  it("ユーザー情報に、十分表示される", () => {
+    const wrapper = mount(UserProfile, {
+      props: {
+        user: consultationData.consultations[0],
+      },
+    });
+    expect(wrapper.find("img").exists()).toBeTruthy();
+    expect(wrapper.find("img").attributes("src")).toBe(
+      consultationData.consultations[0].profilePicture,
+    );
+    expect(wrapper.find("h2").exists()).toBeTruthy();
+    expect(wrapper.find("h2").text()).toBe(
+      consultationData.consultations[0].name,
+    );
   });
 });
